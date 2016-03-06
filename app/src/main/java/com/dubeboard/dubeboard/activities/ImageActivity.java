9ac97876c5a972af0645ba676d0b3569b4caeaac
Edgar Tenesaca <edgartenesaca@gmail.com>
2016-03-06 12:46:17 -0500

    Agregado buscador de imagenes

--- app/src/main/java/com/dubeboard/dubeboard/activities/ImageActivity.java
+++ app/src/main/java/com/dubeboard/dubeboard/activities/ImageActivity.java
@@ -1,14 +1,20 @@
 package com.dubeboard.dubeboard.activities;
 
+import android.app.ProgressDialog;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.text.method.CharacterPickerDialog;
 import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
@@ -18,12 +24,17 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.dubeboard.dubeboard.ListViewDinamicSize;
+import com.dubeboard.dubeboard.ManageDB;
 import com.dubeboard.dubeboard.R;
 import com.dubeboard.dubeboard.clsCategory;
 import com.dubeboard.dubeboard.clsImage;
+import com.dubeboard.dubeboard.gl;
 import com.dubeboard.dubeboard.item.adapter.ImageItem_1;
+import com.dubeboard.dubeboard.item.adapter.ImageItem_3;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ImageActivity extends AppCompatActivity {
@@ -34,9 +45,6 @@ public class ImageActivity extends AppCompatActivity {
     ImageItem_1 adapter;
     GridView dataList;
     String[] menuItems = new String[]{"Editar", "Eliminar"};
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,15 +99,54 @@ public class ImageActivity extends AppCompatActivity {
                 startActivity(ImageActivity);
             }
         });
-
-        // Obtener todas las imagenes de la base de datos
-        final List<clsImage> images = ImageObj.getAll();
-        for(clsImage im : images){
-            ImageList.add(im);
-        }
-        adapter = new ImageItem_1(this, R.layout.list_item_image_1, ImageList);
-        dataList.setAdapter(adapter);
+        LoadListData();
         registerForContextMenu(dataList);
+    }
+
+    void LoadListData(){
+        LoadListData("");
+    }
+    void LoadListData(String TextToSearch){
+        LoadData Task = new LoadData(TextToSearch);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            Task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        } else { Task.execute(); }
+    }
+    protected class LoadData extends AsyncTask<String, Void, String> {
+        ProgressDialog pDialog;
+        List<Object[]> args;
+
+        public LoadData(String TextToSearch) {
+            List<Object[]> args = new ArrayList<Object[]>();
+            if (!TextToSearch.equals("")){
+                args.add(new Object[]{ManageDB.ColumnsImage.IMAGE_NAME, "like",  "%" + TextToSearch + "%"});
+            }
+            this.args = args;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = ProgressDialog.show(ImageActivity.this, "", "Cargando Datos");
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            ImageList.clear();
+            final List<clsImage> images = ImageObj.getRecords(args);
+            for(clsImage im : images){
+                ImageList.add(im);
+            }
+            adapter = new ImageItem_1(Context, R.layout.list_item_image_1, ImageList);
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String res) {
+            super.onPostExecute(res);
+            dataList.setAdapter(adapter);
+            pDialog.dismiss();
+        }
     }
 
     @Override
@@ -133,12 +180,65 @@ public class ImageActivity extends AppCompatActivity {
         return true;
     }
 
+    private Menu menu;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //MenuInflater inflater = getMenuInflater();
-        //inflater.inflate(R.menu.category, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.image, menu);
+        this.menu = menu;
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            SearchView search = (SearchView) menu.findItem(R.id.action_search).getActionView();
+            search.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+            search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    LoadListData(query);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+            search.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus){
+                        LoadListData();
+                    }
+                }
+            });
+        }
         return true;
     }
+
+    // History
+    /*
+    private List<String> items = new ArrayList<>();
+    private void loadHistory(String query) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Cursor
+            String[] columns = new String[] { "_id", "text" };
+            Object[] temp = new Object[] { 0, "default" };
+
+            MatrixCursor cursor = new MatrixCursor(columns);
+            for(int i = 0; i < items.size(); i++) {
+
+                temp[0] = i;
+                temp[1] = items.get(i);//replaced s with i as s not used anywhere.
+                        cursor.addRow(temp);
+            }
+
+            SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            final SearchView search = (SearchView) menu.findItem(R.id.action_search).getActionView();
+            search.setSuggestionsAdapter(new ExampleAdapter(this, cursor, items));
+        }
+    }
+    */
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -149,7 +249,7 @@ public class ImageActivity extends AppCompatActivity {
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
                 return true;
-            case R.id.btnAdd:
+            case R.id.btnSpeech:
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
