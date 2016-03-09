bd5be768b521993d2581bcc659b2d81a31b4276e
Edgar Tenesaca <edgartenesaca@gmail.com>
2016-03-08 23:02:17 -0500

    Optimizada vista de home

--- app/src/main/java/com/dubeboard/dubeboard/activities/HomeActivity.java
+++ app/src/main/java/com/dubeboard/dubeboard/activities/HomeActivity.java
@@ -248,7 +248,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
+        // getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
@@ -260,9 +260,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_add_item) {
+        //if (id == R.id.action_add_item) {
 
-        }
+        //}
         return super.onOptionsItemSelected(item);
     }
 
